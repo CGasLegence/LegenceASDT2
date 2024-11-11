a9c222c1ef8f65779a4febfc8b4d8cfaac32a5fa
Corey Gashlin <Corey.Gashlin@wearelegence.com>
2024-11-11 16:29:52 -0500

    r

--- LegenceASDT2Web/TaskPane/SCRIPT/signature_templates.js
+++ LegenceASDT2Web/TaskPane/SCRIPT/signature_templates.js
@@ -4,27 +4,27 @@
 function get_template_A_str(user_info)
 {
     let str = "";
-    const response = await fetch("../HTML/LegenceSig.html");
-    str = await response.text();
+    //const response = await fetch("../HTML/LegenceSig.html");
+    //str = await response.text();
 
-  //if (is_valid_data(user_info.greeting))
-  //{
-  //  str += user_info.greeting + "<br/>";
-  //}
+  if (is_valid_data(user_info.greeting))
+  {
+    str += user_info.greeting + "<br/>";
+  }
 
-  //str += "<table>";
-  //str +=   "<tr>";
-  //str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='https://officedev.github.io/Office-Add-in-samples/Samples/outlook-set-signature/assets/sample-logo.png' alt='Logo' /></td>";
-  //str +=     "<td style='padding-left: 5px;'>";
-  //str +=	   "<strong>" + user_info.name + "</strong>";
-  //str +=     is_valid_data(user_info.pronoun) ? "&nbsp;" + user_info.pronoun : "";
-  //str +=     "<br/>";
-  //str +=	   is_valid_data(user_info.job) ? user_info.job + "<br/>" : "";
-  //str +=	   user_info.email + "<br/>";
-  //str +=	   is_valid_data(user_info.phone) ? user_info.phone + "<br/>" : "";
-  //str +=     "</td>";
-  //str +=   "</tr>";
-  //str += "</table>";
+  str += "<table>";
+  str +=   "<tr>";
+  str +=     "<td style='border-right: 1px solid #000000; padding-right: 5px;'><img src='https://officedev.github.io/Office-Add-in-samples/Samples/outlook-set-signature/assets/sample-logo.png' alt='Logo' /></td>";
+  str +=     "<td style='padding-left: 5px;'>";
+  str +=	   "<strong>" + user_info.name + "</strong>";
+  str +=     is_valid_data(user_info.pronoun) ? "&nbsp;" + user_info.pronoun : "";
+  str +=     "<br/>";
+  str +=	   is_valid_data(user_info.job) ? user_info.job + "<br/>" : "";
+  str +=	   user_info.email + "<br/>";
+  str +=	   is_valid_data(user_info.phone) ? user_info.phone + "<br/>" : "";
+  str +=     "</td>";
+  str +=   "</tr>";
+  str += "</table>";
 
   return str;
 }
